9511989c127a95eda36ba6475926d99fd86300d2
Marsel <marchello.marsel@gmail.com>
2023-01-04 00:09:55 +0300

    Применен усилитель store (enhancer)

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,29 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import filters from '../reducers/filters';
 import heroes from '../reducers/heroes';
 
+const enhancer =
+	(createStore) =>
+	(...args) => {
+		const store = createStore(...args);
+
+		const oldDispatch = store.dispatch;
+		store.dispatch = (action) => {
+			if (typeof action === 'string') {
+				return oldDispatch({
+					type: action,
+				});
+			}
+
+			return oldDispatch(action);
+		};
+
+		return store;
+	};
+
 const store = createStore(
 	combineReducers({ filters, heroes }),
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	compose(enhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 export default store;
