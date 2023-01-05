1e0736477ddda9270ec5dc754475de500a40562b
Marsel <marchello.marsel@gmail.com>
2023-01-05 17:21:29 +0300

    Реализованны адаптеры с помощью createEntityAdapter

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -2,26 +2,14 @@ import { useHttp } from '../../hooks/http.hook';
 import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { createSelector } from '@reduxjs/toolkit';
 
-import { heroDeleted, fetchHeroes } from './heroesSlice';
+import { heroDeleted, fetchHeroes, filteredHeroesSelector } from './heroesSlice';
 import HeroesListItem from '../heroesListItem/HeroesListItem';
 import Spinner from '../spinner/Spinner';
 
 import './heroesList.scss';
 
 const HeroesList = () => {
-	const filteredHeroesSelector = createSelector(
-		(state) => state.filters.activeFilter,
-		(state) => state.heroes.heroes,
-		(filters, heroes) => {
-			if (filters === 'all') {
-				return heroes;
-			} else {
-				return heroes.filter((item) => item.element === filters);
-			}
-		}
-	);
 	const filteredHeroes = useSelector(filteredHeroesSelector);
 
 	const heroesLoadingStatus = useSelector((state) => state.heroes.heroesLoadingStatus);
